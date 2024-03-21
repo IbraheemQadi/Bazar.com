2c01456ac620b2b22b540cca1551a4ad22e57c5c
Ibraheem Qadi <ibra444hem@gmail.com>
2024-03-21 16:37:10 +0200

    Refactor orderServer - add uuid and jsonfile dependencies - simplify purchase endpoint logic

--- orderServer/index.js
+++ orderServer/index.js
@@ -1,88 +1,38 @@
-const axios = require("axios");
-const { json } = require("body-parser");
 const express = require("express");
-const fs = require("fs");
-app.use(express.json());
-
+const axios = require("axios");
+const { getLog, updateLog } = require("./log/utils");
+const { v4: uuidv4 } = require("uuid");
 const app = express();
-const PORT = 3001;
-let database;
-let jsonData;
 
-// Assuming your JSON file is named 'data.json'
-const filePath = "../database.json";
+const PORT = 3002;
+const CATALOG_SERVER_URL = "http://localhost:3001";
 
-// Read the file asynchronously
+app.get("/", (req, res) => {
+  res.send("Order server is running");
+});
 
-fs.readFile("../database.json", "utf8", (err, data) => {
-  if (err) {
-    console.error("Error reading file:", err);
-    return;
-  }
+app.post("/purchase/:id", async (req, res) => {
+  const bookId = parseInt(req.params.id);
+  const requestURL = `${CATALOG_SERVER_URL}/purchase/${bookId}`;
 
   try {
-    // Parse the JSON data
-    jsonData = JSON.parse(data);
-    database = Object.values(jsonData);
+    const response = await axios.post(requestURL);
 
-    // Now 'jsonData' contains the contents of your JSON file
-    //console.log(jsonData);
-  } catch (error) {
-    console.error("Error parsing JSON data:", error);
+    const order = {
+      orderNumber: uuidv4(),
+      bookId,
+      date: new Date(),
+    };
+
+    const log = await getLog();
+    log.push(order);
+    await updateLog(log);
+    res.json(response.data);
+  } catch (err) {
+    res.status(err.response?.status).send(err.response?.data);
   }
 });
 
-app.put("/ORDER_WEBSERVICE_IP/purchase/:id", (req, res) => {
-  const found = database.some(
-    (database) => database.ID === parseInt(req.params.id)
-  );
-  if (found) {
-    const dataToSend = parseInt(req.params.id);
-    const serverUrl = "http://localhost:3005/cheack_id";
-
-    axios
-      .post(serverUrl, {
-        number: dataToSend,
-      })
-      .then((response) => {
-        if (response.data) {
-          //  console.log("Data sent successfully:", dataToSend, response.data);
-          // decremnt catalog
-          database.forEach((Element) => {
-            if (Element.ID === dataToSend) {
-              Element.quantity = Element.quantity - 1;
-
-              // res.json({ msg: "book update ", Element });
-              // console.log(database);
-            }
-          });
-          //update
-          const updmemper = req.body;
-          database.forEach((Element) => {
-            if (Element.ID === parseInt(req.params.id)) {
-              Element.price = updmemper.price ? updmemper.price : Element.price;
-              Element.quantity = updmemper.quantity
-                ? updmemper.quantity
-                : Element.quantity;
-              res.json({ msg: "book update ", Element });
-              console.log(database);
-
-              fs.writeFileSync("../database.json", JSON.stringify(database));
-            }
-          });
-        }
-
-        // Send the response here inside the Axios .then() block
-      })
-      .catch((error) => {
-        console.error("Error sending data:", error);
-        // Send an error response if there's an issue with Axios request
-        res.status(500).json({ error: "Error sending data" });
-      });
-  } else {
-    res.json("there are no id same u send");
-  }
-});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
